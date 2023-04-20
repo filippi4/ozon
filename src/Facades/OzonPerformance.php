13935c86cbfdc894fcb57c078fb085d03d136a7e
filippovk997 <filippovk997@mail.ru>
2023-04-20 22:31:31 +0400

    add doc method getStatisticsDaily to facade

--- src/Facades/OzonPerformance.php
+++ src/Facades/OzonPerformance.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static mixed getCampaign(array $campaign_ids, string $adv_object_type, string $state)
  * @method static mixed getCampaignObjects(int $campaign_id)
  * @method static mixed getStatisticsExpense(int $campaigns, Carbon $dateFrom, Carbon $dateTo)
+ * @method static mixed getStatisticsDaily(array $campaignId, Carbon $dateFrom, Carbon $dateTo)
  **/
 
 class OzonPerformance extends Facade
